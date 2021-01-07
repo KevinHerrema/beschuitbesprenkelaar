5f96331a382bdace79b622ccf1b18f709d3b1e96
KevinHerrema <kevin.herrema@student.nhlstenden.com>
2021-01-07 14:48:03 +0100

    de xamarin app
    kan een bericht naar de arduino sturen

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        string lampstatus= "off";
+        client test = new client();
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void connect_Clicked(object sender, EventArgs e)
+        {
+            if(lampstatus == "off")
+            {
+                lampstatus = "on";
+            }
+            else
+            {
+                lampstatus = "off";
+            }
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,ProtocolType.Udp);
+
+            IPAddress serverAddr = IPAddress.Parse("192.168.1.36");
+
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, 11000);
+
+            string text = lampstatus;
+            byte[] send_buffer = Encoding.ASCII.GetBytes(text);
+
+            sock.SendTo(send_buffer, endPoint);
+        }
+        private void disconnect_Clicked(object sender, EventArgs e)
+        {
+            test.CloseConnection();
+        }
+
+       
+
+    }
+}
---
+++ App1/App1/App1/client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace App1
+{
+    class client
+    {
+        public static Socket sender;
+        public static IPAddress ipAddress = IPAddress.Parse("192.168.178.47");
+        public static EndPoint remoteEP;
+        public static byte[] bytes = new byte[1024];
+
+        public void OpenConnection()
+        {
+            try
+            {
+                remoteEP = new IPEndPoint(ipAddress, 80);
+                sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sender.Connect(remoteEP);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void CloseConnection()
+        {
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+
+        public void Send(string message)
+        {
+            try
+            {
+                byte[] msg = Encoding.ASCII.GetBytes(message);
+                sender.Send(msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+        public string Receive()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Send("Hallo");
+            int bytesRec = sender.Receive(bytes);
+            return Encoding.ASCII.GetString(bytes, 0, bytesRec);
+        }
+    }
+}
